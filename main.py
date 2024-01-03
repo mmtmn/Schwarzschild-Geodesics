286a88a3c910d79b53d8536e2fc0cef0bf9dc560
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2024-01-03 04:21:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # Define constants globally
 G = 6.67430e-11  # Gravitational constant
@@ -30,7 +31,7 @@ def calculate_time_dilation(r, M):
     return np.sqrt(1 - rs / r)
 
 def update_particle(particle, M, dt):
-    y0 = [particle['position'], particle['velocity'], 0, 0, 0]
+    y0 = [particle['position'], particle['velocity'], particle['phi'], particle['pphi'], 0]
     sol = solve_ivp(schwarzschild_geodesic, [0, dt], y0, args=(M,), method='RK45')
 
     if len(sol.y[0]) > 0:
@@ -38,31 +39,43 @@ def update_particle(particle, M, dt):
         particle['velocity'] = sol.y[1][-1]
         particle['proper_time'] += sol.y[4][-1]
 
+    particle['phi'] = sol.y[2][-1] 
     particle['time_dilation'] = calculate_time_dilation(particle['position'], M)
 
     return particle
 
 # Main simulation loop with visualization
 running = True
-particles = [{'position': 1e11, 'velocity': 0, 'proper_time': 0}]
+initial_pphi = 1e12 
+particles = [{'position': 1e11, 'velocity': 0, 'proper_time': 0, 'phi': 0, 'pphi': initial_pphi}]
 star_mass = 1.989e+30
 dt = 100
 iterations = 0
 particle_positions = []
+particle_phis = []
 
-while running and iterations < 500:  # Increase iterations for a better plot
+
+while running and iterations < 500:
     for particle in particles:
         particle = update_particle(particle, star_mass, dt)
         particle_positions.append(particle['position'])
+        particle_phis.append(particle['phi'])  # Store the current phi value
         if particle['position'] < 2 * G * star_mass / c**2:
             running = False
     iterations += 1
 
-# Plotting the particle's position over time
-plt.figure(figsize=(10, 6))
-plt.plot(particle_positions)
-plt.xlabel('Time Steps')
-plt.ylabel('Distance from Star (meters)')
-plt.title('Particle Falling Towards a Star Over Time')
-plt.grid(True)
+
+# After simulation, for plotting
+fig = plt.figure(figsize=(10, 6))
+ax = fig.add_subplot(111, projection='3d')
+
+x_positions = [r * np.sin(phi) for r, phi in zip(particle_positions, particle_phis)]
+y_positions = [r * np.cos(phi) for r, phi in zip(particle_positions, particle_phis)]
+z_positions = [0] * len(x_positions)  # Assuming motion in a plane
+
+ax.plot(x_positions, y_positions, z_positions)
+ax.set_xlabel('X Coordinate')
+ax.set_ylabel('Y Coordinate')
+ax.set_zlabel('Z Coordinate')
+plt.title('3D Trajectory of a Particle in Schwarzschild Spacetime')
 plt.show()
